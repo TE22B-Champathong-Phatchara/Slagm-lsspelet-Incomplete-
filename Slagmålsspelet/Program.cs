93154000bf469b4bcefe4b843e8b8a9d2a32b1b6
Phatchara Champathong <Phatchara.Champathong@elev.ga.ntig.se>
2023-10-04 10:08:26 +0200

    asdas

--- Slagmålsspelet/Program.cs
+++ Slagmålsspelet/Program.cs
@@ -28,11 +28,10 @@ Console.WriteLine("Good luck!!");
 Console.WriteLine("__________________________");
 while (true)
 {
-    
     Random generator = new Random();
-    int hp1 = generator.Next(40,100);
-    int hp2 = generator.Next(40,100);
-    
+    int hp1 = generator.Next(40, 100);
+    int hp2 = generator.Next(40, 100);
+
     Console.WriteLine("Write the P1 name:");
     p1 = Console.ReadLine();
     Console.WriteLine("Write the P2 name:");
@@ -40,61 +39,56 @@ while (true)
     Thread.Sleep(500);
     Console.WriteLine("Get ready " + p1 + " and " + p2 + "!");
     Console.WriteLine("__________________________");
-    Console.WriteLine(""+ p1 + "'s HP: " + hp1 +"" );
-    Console.WriteLine(""+ p2 + "'s HP: " + hp2 +"" );
+    Console.WriteLine(p1 + "'s HP: " + hp1);
+    Console.WriteLine(p2 + "'s HP: " + hp2);
     Thread.Sleep(1000);
-    Console.WriteLine(""+ p1 +", your turn to start!!");
-    while(true)
+    Console.WriteLine(p1 + ", your turn to start!!");
+
+    while (true)
     {
-        int dmg1 = generator.Next(10,75);
-        int dmg2 = generator.Next(10,75);
+        int dmg1 = generator.Next(10, 75);
+        int dmg2 = generator.Next(10, 75);
         Console.ReadKey();
-        if (hp2 > dmg1)
-        {
-            hp2 = hp2 - dmg1;
-        }
-        else if (hp2 < dmg1)
-        {
-            hp2 = dmg1 - hp2;
-        }
-        else
-        {
-            continue;
-        }
-        Console.WriteLine("" + p1 + " took a straight pucnh!!");
-        if (hp2 > 0)
-        {
-            Console.WriteLine("" + p2 + "'s HP is now: " + hp2 +"!");
-            Thread.Sleep(1000);
-            Console.WriteLine("It's " + p2 + "'s turn now!" );
-            Console.ReadKey();
-            if (hp1 > dmg2)
-            {
-                hp1 = hp1 - dmg2;
-            }
-            else if (hp1 < dmg2)
-            {
-                hp1 = dmg2 - hp1;
-            }
-            Console.WriteLine("" + p1 + "'s HP is now: " + hp1 +"!");
-            Thread.Sleep(1000);
-            Console.WriteLine("It's " + p1 + "'s turn now!" );
-            continue;
-        }
-        else if (hp2 <= 0)
-        {
-            Console.WriteLine("" + p2 + " was knocked down!" + p1 + " is the winner!");
-            break;
-        }
-        else if (hp1 <= 0)
-        {
-            Console.WriteLine("" + p1 + " was knocked down!" + p2 + " is the winner!");
-            break;
-        }
+
         
+        hp2 -= dmg1;
+        Console.WriteLine(p1 + " took a straight punch!!");
+        Console.WriteLine(p2 + "'s HP is now: " + hp2 + "!");
+
+        if (hp2 <= 0)
+        {
+            Console.WriteLine(p2 + " was knocked down! " + p1 + " is the winner!");
+            break;
+        }
+
+        Console.WriteLine("It's " + p2 + "'s turn now!");
+        Console.ReadKey();
+
+        
+        hp1 -= dmg2;
+        Console.WriteLine(p2 + " took a straight punch!!");
+        Console.WriteLine(p1 + "'s HP is now: " + hp1 + "!");
+
+        if (hp1 <= 0)
+        {
+            Console.WriteLine(p1 + " was knocked down! " + p2 + " is the winner!");
+            break;
+        }
+
+        Console.WriteLine("It's " + p1 + "'s turn now!");
     }
 
-    
+    Console.WriteLine("Restart? (Y/N)");
+    string yn = Console.ReadLine().ToLower();
+    if (yn == "y")
+    {
+        continue;
+    }
+    else if (yn == "n")
+    {
+        break;
+    }
+    Console.ReadKey();
     break;
 }
 
@@ -125,10 +119,3 @@ while (true)
 
 
 
-
-
-
-
-Console.WriteLine("");
-Console.WriteLine("Press any key to exit.");
-Console.ReadKey();
